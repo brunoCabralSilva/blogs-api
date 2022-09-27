d2f1319d973f405eeb5b880d120cc89c96e435c4
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-09-27 15:00:17 -0300

    Requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+const CatSchema = (sequelize, DataTypes) => {
+    const CatTable = sequelize.define('Category', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING(255),
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    });
+   
+    return CatTable;
+  };
+  
+  module.exports = CatSchema;
