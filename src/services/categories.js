2702f50fda2bc7a59136c024ab1c7d2ab6fe9625
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-09-27 15:32:38 -0300

    Requisito 09 com correção no lint

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,15 +1,16 @@
 const { Category } = require('../models');
+
 const register = async (name) => {
   await Category.create({ name });
   const add = await Category.findOne({
-    where: { name }
+    where: { name },
   });
   return add.dataValues;
-}
+};
 
 const getAllCategories = async () => {
   const query = await Category.findAll();
   return query;
-}
+};
 
 module.exports = { register, getAllCategories };
